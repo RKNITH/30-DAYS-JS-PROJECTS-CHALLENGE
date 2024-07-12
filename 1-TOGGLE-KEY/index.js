3d6deb9c6bb4bf4bd16d0ad822959001a71bb5aa
RKNITH <raviranjankofficial@gmail.com>
2024-07-12 06:19:38 +0530

    first commit

---
+++ 1-TOGGLE-KEY/index.js
@@ -0,0 +1,14 @@
+const wrong = document.getElementById("wrong")
+const right = document.getElementById("right")
+
+
+wrong.addEventListener('click', () => {
+    right.style.display = ' flex'
+    wrong.style.display = 'none'
+
+})
+right.addEventListener('click', () => {
+    right.style.display = ' none'
+    wrong.style.display = 'flex'
+
+})
